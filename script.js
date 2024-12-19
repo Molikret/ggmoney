1eaa4815a65763c217d6b41ea4f0a9aaf55de855
Molikret <127600324+Molikret@users.noreply.github.com>
2024-12-19 13:31:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Passwort-Nachrichten-Kombinationen
 const secrets = {
-    "9gW34e": "5x7zT - Code | 50.000$ - Gewinn",
     "password123": "Das ist deine geheime Nachricht für Passwort123!",
     "helloWorld": "Hier ist eine andere geheime Nachricht!",
     "openSesame": "Das Passwort öffnet das Geheimnis!",
@@ -12,7 +11,7 @@ function checkPassword() {
 
     if (secrets[passwordInput]) {
         messageDiv.textContent = secrets[passwordInput];
-        messageDiv.style.color = 'green';
+        messageDiv.style.color = '#00ffcc';
     } else {
         messageDiv.textContent = "Falsches Passwort! Versuche es erneut.";
         messageDiv.style.color = 'red';
